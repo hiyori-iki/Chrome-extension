7a647782e61f3d67dc84e69a133fe8f8b0290428
hiyori-iki <dhannugg@gmail.com>
2022-10-20 23:44:13 +0530

    Updated

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function buttonClick()
 {
     console.log("Button clicked");
 }
+let saveInput=document.getElementById("input-btn")
+
+saveInput.addEventListener('click',()=>{
+    console.log("Clicked")
+})
