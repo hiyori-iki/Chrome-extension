c06020433cf85beb7bbdc5d6b0bffacca5844402
hiyori-iki <dhannugg@gmail.com>
2022-10-21 09:08:35 +0530

    Updated

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-let saveInput=document.getElementById("input-btn")
-
-saveInput.addEventListener('click',()=>{
-    console.log("Clicked")
+let myLeads=[]
+let i=0
+const inputBtn=document.getElementById("input-btn")
+const ulEl=document.getElementById("ul-el")
+inputBtn.addEventListener('click',()=>{
+    const inputEl=document.getElementById("input-el")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+    const li = document.createElement("li")
+    li.textContent = myLeads[i]
+    ulEl.append(li)
+    i++;
 })
