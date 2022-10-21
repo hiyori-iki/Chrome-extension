edb56b8aa24f0097193634c573dd030b04758379
hiyori-iki <dhannugg@gmail.com>
2022-10-21 11:10:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 let myLeads=[]
 let i=0
+let isEmpty=true;
 const inputBtn=document.getElementById("input-btn")
 const ulEl=document.getElementById("ul-el")
 inputBtn.addEventListener('click',()=>{
     const inputEl=document.getElementById("input-el")
-    myLeads.push(inputEl.value)
-    console.log(myLeads)
-    renderleads()
+    if(inputEl.value!='')
+    {
+        myLeads.push(inputEl.value)
+        console.log(myLeads)
+        renderleads()
+    }
     inputEl.value=""
 })
 function renderleads()
